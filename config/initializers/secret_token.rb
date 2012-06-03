6ff5ff1d229bf78943b5f0fae55c5b121317e086
javier velazquez <javier.velazquez@crowdint.com>
2012-06-03 07:57:08 -0500

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Images::Application.config.secret_token = 'ff02bacb7be672b1bf18976084a4536cd30b0e8d150983ef08fb056dc2d5542532b89d08e3f686c2b7f8136dad948efd4c15bc98c8020ea56a0f507a90bfc344'
